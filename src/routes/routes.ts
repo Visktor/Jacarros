bf2d89324e32db369f4da23e9f92b179126459e8
Vitor Barros <webdev.vitor@gmail.com>
2024-02-14 18:42:04 -0300

    Creates base router file

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,31 @@
 import { Router } from "express";
+import zodValidateObject from "../shared/validation/zodObjectValidator";
+import z from "zod";
 
-const router = Router()
+const router = Router();
 
-router.use()
+router.use("/", async (req, res) => {
+  res.send("PrimeCars Api v0.1");
+});
+
+router.use("/login", async (req, res) => {
+  const { email, password } = req.query;
+
+  const validationResult = zodValidateObject(
+    { email, password },
+    {
+      email: z.string().email(),
+      password: z.string(),
+    }
+  );
+
+  if (!validationResult.success) {
+    res.json({
+      success: false,
+      message: "Email ou senha inválidos, tente novamente mais tarde.",
+      erro: validationResult.error,
+    });
+  }
+
+
+});
