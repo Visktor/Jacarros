80f841e0102a6a743e363fc171f1c309039d7555
Vitor Barros <webdev.vitor@gmail.com>
2024-02-14 18:40:00 -0300

    Creates zod validation function
    This function accepts an object and a zod schema as parameters and returns the zod validation results

---
+++ src/shared/validation/zodObjectValidator.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export default function zodValidateObject<
+  O extends { [key: string]: unknown },
+  S extends { [K in keyof O]: z.ZodTypeAny }
+>(
+  obj: O,
+  zodSchema: S
+):
+  | { success: false; error: z.ZodError }
+  | { success: true; data: z.infer<z.ZodObject<S>> } {
+  return z.object(zodSchema).safeParse(obj);
+}
